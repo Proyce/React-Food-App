4b2f8f1ede78c1fd0cec0ed039a0c7ab48358e00
proyce <prospernnadi3@gmail.com>
2022-08-09 01:56:48 +0100

    Final commit

---
+++ src/components/RecipeList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
+import "./RecipeList.css";
+
+export default function RecipeList({ recipes }) {
+  const {mode} = useTheme()
+  if (!recipes.length) return <div className="error"> No recipes to load.</div>;
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className={`card ${mode}`}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to prepare.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
